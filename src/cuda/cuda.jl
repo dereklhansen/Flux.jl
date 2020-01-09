871fcd5fc05b6432871c9c4c01c3073ea1fd342f
Derek Hansen <dereklh@umich.edu>
2020-01-08 21:44:05 -0500

    just commenting out cudnn functionality for now

--- src/cuda/cuda.jl
+++ src/cuda/cuda.jl
@@ -2,11 +2,11 @@ module CUDA
 
 using ..CuArrays
 
-if CuArrays.has_cudnn()
-  include("curnn.jl")
-  include("cudnn.jl")
-else
+#= if CuArrays.has_cudnn() =#
+#=   include("curnn.jl") =#
+#=   include("cudnn.jl") =#
+#= else =#
   @warn "CUDNN is not installed, some functionality will not be available."
-end
+#= end =#
 
 end
